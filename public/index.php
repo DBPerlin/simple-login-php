e542849f316450a3030973414cfe501a4b140059
Manuella Dal Bianco Perlin Almeida <manuella.perlin@cfn.local>
2026-02-26 13:04:07 -0300

    First commit

---
+++ public/index.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+
+$rotas_permitidas = require_once __DIR__ . '/../inc/rotas-php'; 
+
+$rota = $_GET['rota'] ?? 'home';
+
+if(!isset($_SESSION['usuario'])){
+    $rota = 'login';
+}
+
+if(isset($_SESSION['usuario'] && $rota == 'login')){
+    $rota = 'home';
+}
+
+if(!in_array($rota, $rotas_permitidas)){
+    $rota = '404';
+}
+
+$script = null;
+switch ($rota) {
+    case '404':
+        $script = '404.php';
+        break;
+        
+    case 'login':
+        $script = 'login.php';
+        break;
+    
+    case 'home':
+        $script = 'home.php';
+        break;
+}
+
+require_once __DIR__ . "/../inc/header.php";
+require_once __DIR__ . "/../scripts/$script";
+require_once __DIR__ . "/../inc/footer.php";
+
+
